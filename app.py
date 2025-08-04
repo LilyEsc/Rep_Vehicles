61134a52121669206ba19fc9a278f9a46bfd87c6
LilyAnn <lian.scbr@gmail.com>
2025-08-04 02:17:21 -0600

    import pandas as pd import plotly.graph_objects as go # Importación de plotly.graph_objects como go
    # Leer los datos del archivo CSV
    car_data = pd.read_csv('vehicles_us.csv')
    
    # Crear un histograma utilizando plotly.graph_objects
    # Se crea una figura vacía y luego se añade un rastro de histograma
    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
    
    # Opcional: Puedes añadir un título al gráfico si lo deseas
    fig.update_layout(title_text='Distribución del Odómetro')
    
    # Mostrar el gráfico Plotly
    fig.show()

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('C:/Users/hp/Documents/ProyectoSprint7/Rep_Vehicles/vehicles_data.csv')
+
+st.title('Proyecto Sprint 7. Analisis sobre venta de autos')
+
+st.header('Dataset de Vehiculos')
+st.dataframe(car_data)
+
+hist_button = st.checkbox('Construir histograma')
+
+if hist_button: #al hacer click en el botón
+    st.write(f'Histograma por rango de precios de los vehiculos')
+    fig = px.histogram(car_data, x='price')
+    st.plotly_chart(fig, use_container_width=True)
+
+barra_button = st.checkbox('Constriur Gráfico de Barras')
+
+if barra_button:
+    tipo_auto = car_data.groupby(by='type')['price'].agg('count')
+    tipo_auto = tipo_auto.reset_index()
+    st.write('Creación de un Gráfico de Barras para los diferente tipos de carroceria')
+    fig = px.bar(tipo_auto, x='type', y='price', color='type')
+    st.plotly_chart(fig, use_container_width=True)
+
+marcas = ['ford', 'chevrolet', 'toyota', 'nissan', 'hyundai']
+marca_select = st.multiselect('Seleccione una marca:', options=marcas)
+
+"""
+masc = pd.Series([False] * len(car_data))
+for marca in marcas_mostrar:
+    masc = masc | car_data['model'].str.contains(marca, case=False, na=False)
+
+car_marcas = car_data[masc]
+
+st.subheader('Tipos de Carroceria por Fabricante')
+
+
+
+fig = px.line(car_marcas, x=car_marcas['type'])
+#tipo = car_data.groupby('model')['type'].value_counts()
+#st.bar_chart
+"""
